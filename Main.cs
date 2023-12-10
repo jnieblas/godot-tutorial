5875b170f0303ac62b1d23667803f7161e78208a
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-12-10 13:49:15 -0700

    Adding first Scenes & Buttons

---
+++ Main.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public partial class Main : Node2D {
+	private void OnQuitPressed() {
+		GetTree().Quit();
+	}
+
+	private void OnPlayPressed() {
+		GetTree().ChangeSceneToFile("res://world.tscn");
+	}
+
+}
